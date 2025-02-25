b858529ddddb77897df40be7dc3e04c7cf1d4bf1
Amir Alsayed <amyralsyd367@gmail.com>
2025-02-25 19:18:17 +0200

    starting data collection niceone

---
+++ niceonesa/importantUrls.js
@@ -0,0 +1,3 @@
+const url = "https://api.niceonesa.com/?route=rest%2Fproduct_admin%2Fproducts&seo_url=makeup&sort=most_popular&manufacturer_ids&attribute_ids&search&page=1&limit=100"
+
+// x-oc-restadmin-id c15378d0-04f1-4d36-9af7-ab7e17da918b << header.
